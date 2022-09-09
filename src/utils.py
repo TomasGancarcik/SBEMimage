64291c246d9dab9a511219849491cb913be56b68
Ganci <gancarcik.tomas@gmail.com>
2022-09-09 15:22:10 +0200

    utils.py: fixed missing skimage.measure.blur_effect module for sharpness evaluation

--- src/utils.py
+++ src/utils.py
@@ -28,7 +28,7 @@ from logging.handlers import RotatingFileHandler
 from shapely.geometry import Polygon
 from shapely.geometry import Point
 from skimage.transform import ProjectiveTransform
-from skimage.measure import ransac
+from skimage.measure import ransac, blur_effect
 from serial.tools import list_ports
 from PyQt5.QtCore import QObject, pyqtSignal
 
